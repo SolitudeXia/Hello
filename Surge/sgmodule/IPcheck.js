35458a84a60acf8e0ac213a0b0d203daf641c782
stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>
2023-03-11 12:20:16 +0800

    Create IPcheck.js
    Signed-off-by: stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>

---
+++ Surge/sgmodule/IPcheck.js
@@ -0,0 +1,27 @@
+# > copy自LucaLin233
+
+let url = "http://ip-api.com/json/?lang=zh-CN"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "节点详情",
+    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}

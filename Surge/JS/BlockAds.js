09bc9cd45c969bdb405e488bd3f8291fc1eb89c7
stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>
2023-12-07 18:18:56 +0800

    Update BlockAds.js
    Signed-off-by: stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>

--- Surge/JS/BlockAds.js
+++ Surge/JS/BlockAds.js
@@ -1,12 +1,7 @@
 let url = $request.url
 let body = null
 
-if (url.includes("mp.weixin.qq.com/mp/getappmsgad")) {  // 微信公众号
-    let obj = JSON.parse($response.body)
-    obj["advertisement_num"] = 0
-    obj["advertisement_info"] = []
-    body = JSON.stringify(obj)
-} else if (url.includes("amdc/mobileDispatch")) {  // 高德地图 | 菜鸟
+if (url.includes("amdc/mobileDispatch")) {  // 高德地图 | 菜鸟
     let header = $request.headers
     let ua = header["User-Agent"] || header["user-agent"]
     if (ua.includes("AMap") || ua.includes("Cainiao")) {

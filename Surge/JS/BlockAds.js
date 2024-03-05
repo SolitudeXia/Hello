0d877b6329b310192f7a7076b0991e1cef1c6b0e
stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>
2024-03-05 18:33:47 +0800

    Update BlockAds.js
    Signed-off-by: stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>

--- Surge/JS/BlockAds.js
+++ Surge/JS/BlockAds.js
@@ -9,7 +9,9 @@ const isQuanX = typeof $task !== "undefined";
 if (url.includes("/amdc/mobileDispatch")) {
   if (
     ua.includes("AMapiPhone") || // 高德地图
+    ua.includes("Alibaba") || // 阿里巴巴
     ua.includes("Cainiao4iPhone") || // 菜鸟
+    ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
   ) {
     if (isQuanX) {
       $done({ status: "HTTP/1.1 404 Not Found" });

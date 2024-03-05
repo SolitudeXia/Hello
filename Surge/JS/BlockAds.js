769c28f583ee565e56ba022a7c0d2f0366cd1b4d
stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>
2024-03-05 14:51:23 +0800

    Update BlockAds.js
    Signed-off-by: stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>

--- Surge/JS/BlockAds.js
+++ Surge/JS/BlockAds.js
@@ -1,19 +1,53 @@
-let url = $request.url
-let body = null
+// 2024-02-20 17:20
 
-if (url.includes("amdc/mobileDispatch")) {  // 高德地图 | 菜鸟
-    let header = $request.headers
-    let ua = header["User-Agent"] || header["user-agent"]
-    if (ua.includes("AMap") || ua.includes("Cainiao")) {
-        if ('undefined' !== typeof $task) $done({status: 'HTTP/1.1 404 Not Found'})
-        else $done()
-    } else $done({})
-} else {
-    console.log("匹配到其他url：\n" + url)
-}
+const url = $request.url;
+const header = $request.headers;
+const opt12306 = header["Operation-Type"] || header["operation-type"];
+const ua = header["User-Agent"] || header["user-agent"];
 
-if (body) {
-    $done({body})
+if (url.includes("/amdc/mobileDispatch")) {
+  if (
+    ua.includes("AMapiPhone") || // 高德地图
+    ua.includes("Cainiao4iPhone") || // 菜鸟
+  ) {
+    if (typeof $task !== "undefined") {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done();
+    }
+  } else {
+    $done({});
+  }
+} else if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
+  const list12306 = [
+    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
+    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
+    // "com.cars.otsmobile.city",
+    // "com.cars.otsmobile.initCountry",
+    // "com.cars.otsmobile.initNewSysCache",
+    // "com.cars.otsmobile.initProvince",
+    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
+    "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
+    // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
+    "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
+    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
+    "com.cars.otsmobile.newHomePageBussData" // 商品信息流
+    // "com.cars.otsmobile.newHomePageRefresh",
+    "com.cars.otsmobile.travelPage.initData", // 出行服务
+  ];
+  if (typeof $task !== "undefined") {
+    if (list12306?.includes(opt12306)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (list12306?.includes(opt12306)) {
+      $done();
+    } else {
+      $done({});
+    }
+  }
 } else {
-    $done({})
+  $done({});
 }

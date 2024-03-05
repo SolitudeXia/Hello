8566b9ca21331407e2d8e0baf2df67db965910f6
stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>
2024-03-05 17:11:23 +0800

    Update BlockAds.js
    Signed-off-by: stardustXHJie <91548134+stardustXHJie@users.noreply.github.com>

--- Surge/JS/BlockAds.js
+++ Surge/JS/BlockAds.js
@@ -29,11 +29,11 @@ if (url.includes("/amdc/mobileDispatch")) {
     "com.cars.otsmobile.integration.activityBanner", // 活动横幅
     "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
     // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
-    "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
+    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
     "com.cars.otsmobile.newHomePage.initData", // 热门资讯
     "com.cars.otsmobile.newHomePageBussData" // 商品信息流
     // "com.cars.otsmobile.newHomePageRefresh",
-    "com.cars.otsmobile.travelPage.initData", // 出行服务
+    // "com.cars.otsmobile.travelPage.initData", // 出行服务
   ];
   if (typeof $task !== "undefined") {
     if (list12306?.includes(opt12306)) {
